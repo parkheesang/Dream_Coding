3cfe78ba999817606111e7b8fc5cdd21f6448fb1
Edward <74940337+parkheesang@users.noreply.github.com>
2020-12-24 10:07:09 +0900

    Add files via upload

--- xmas/main.js
+++ xmas/main.js
@@ -9,3 +9,17 @@ function go_rtan() {
   alert('숨겨진 메세지 발견!!')
   window.location.href='http://www.google.com';
 }
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function divide(num1, num2) {
+  return num1/num2;
+}
+function surprise(operator) {
+    const result = operator(2, 3);
+    console.log(result);
+}
+
+surprise(divide);
